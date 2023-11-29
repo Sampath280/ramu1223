45903249e39d9c57c9e45162cd4b4deed50ca9de
Sampath Pujari (MINDTREE LIMITED) <v-sapujari@microsoft.com>
2023-11-29 18:44:04 +0530

    Initial commit

---
+++ api/getTitleFromApi/index.js
@@ -0,0 +1,43 @@
+const sql = require('mssql');
+
+const config = {
+  server: 'sampatg56.database.windows.net',
+  database: 'sampath7889',
+  user: 'sampath',
+  password: 'Ra@80muravi',
+  options: {
+    encrypt: true,
+    trustServerCertificate: false,
+    connectTimeout: 30000,
+  },
+};
+
+module.exports = async function (context, req) {
+  const pool = new sql.ConnectionPool(config);
+
+  try {
+    await pool.connect();
+    context.log('Connected to SQL Server');
+
+    // Execute a simple SQL query to fetch data
+    const result = await pool.request().query('SELECT ID, Name, Age FROM [dbo].[MyTable]');
+
+    // Send the fetched data in the response
+    context.res = {
+      status: 200,
+      body: result.recordset,
+      contentType: 'application/json',
+    };
+  } catch (err) {
+    context.log.error('Database connection error:', err);
+    context.res = {
+      status: 500,
+      body: 'Internal Server Error',
+    };
+  } finally {
+    if (pool.connected) {
+      await pool.close();
+      context.log('Closed SQL Server connection');
+    }
+  }
+};
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7071',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
